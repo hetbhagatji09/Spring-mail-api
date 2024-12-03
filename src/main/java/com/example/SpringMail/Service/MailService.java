947dfc6d0be76033d96b7abd0fa97575960d30e8
hetbhagatji09 <hetbhagatji09@gmail.com>
2024-12-03 19:36:35 +0530

    implement method for otp

--- src/main/java/com/example/SpringMail/Service/MailService.java
+++ src/main/java/com/example/SpringMail/Service/MailService.java
@@ -5,6 +5,8 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
+import java.util.Random;
+
 @Service
 public class MailService {
     @Autowired
@@ -12,11 +14,18 @@ public class MailService {
 
     public String sendEmail(String to, String subject, String bodyTestMail) {
         SimpleMailMessage message = new SimpleMailMessage();
+
         message.setFrom("hetbhagatji09@gmail.com"); // Replace with your email or dynamically use the username
         message.setTo(to);
         message.setSubject(subject);
+        bodyTestMail=bodyTestMail+"\n\n"+"Your OTP is "+generateOtp();
         message.setText(bodyTestMail);
         javaMailSender.send(message);
         return "Email sent";
+    }
+    private String generateOtp() {
+        Random random = new Random();
+        int otp = 100000 + random.nextInt(900000); // Generate a number between 100000 and 999999
+        return String.valueOf(otp);
     }
 }
